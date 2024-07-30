ceb882df250266440890d11f9b8d4fc255c53b13
Carter Pfaff <79235133+MelonMars@users.noreply.github.com>
2024-07-29 21:36:14 -0400

    1hr
    Decided to pivot to making some cells.

--- blot2.js
+++ blot2.js
@@ -1,8 +1,10 @@
 const width = 125;
 const height = 125;
-const points = 4;
-const pointRadius = 1;
+const points = 8;
+const nucleusRadius = 1;
 const pointSteps = 60;
+const numberMitochondria = 2;
+const mitochondriaDistance = 5;
 
 setDocDimensions(width, height);
 
@@ -23,9 +25,37 @@ function drawCircle(pt, col, r) {
   drawLines(ptDrawer.lines(), {"fill": col});
 }
 
+function grahamScan(pts) {
+  let stack = []
+  pts.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+  function orientation(p, q, r) {
+        return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]);
+  }   
+  const lower = [];
+    for (const p of pts) {
+        while (lower.length >= 2 && orientation(lower[lower.length - 2], lower[lower.length - 1], p) <= 0) {
+            lower.pop();
+        }
+        lower.push(p);
+    }
+  const upper = [];
+    for (let i = pts.length - 1; i >= 0; i--) {
+        const p = pts[i];
+        while (upper.length >= 2 && orientation(upper[upper.length - 2], upper[upper.length - 1], p) <= 0) {
+            upper.pop();
+        }
+        upper.push(p);
+    }
+  lower.pop();
+  upper.pop();
+  return lower.concat(upper);
+}
+
 const pts = []
+const lines = []
 for (let i=0; i<points; i++) {
   pts.push([bt.rand() * width, bt.rand() * height]);
+  lines.push([])
 }
 
 const possibleColors = [
@@ -40,6 +70,7 @@ const possibleColors = [
     "#00FFFF",
     "#FFC0CB"
 ];
+
 const ptColors = []
 for (let i=0; i<points;i++) {
   const index = bt.randIntInRange(0, possibleColors.length-1);
@@ -58,10 +89,59 @@ for (let w=0;w<width;w++) {
         closestDistance = distance;
       }
     }
-    drawCircle([w, h], ptColors[pts.indexOf(closestPoint)], 0.5)
+    lines[pts.indexOf(closestPoint)].push([w,h])
+  }
+}
+
+for (const tessel of lines) {
+  const tess = new bt.Turtle()
+  tess.up()
+  tess.goTo(tessel[0])
+  tess.down()
+  for (const pt of grahamScan(tessel)) {
+    tess.goTo(pt);
+  }
+  drawLines(tess.lines())
+}
+
+for (let tessel of lines) {
+  const tess = new bt.Turtle()
+  tess.down()
+  tessel = grahamScan(tessel)
+  let pts = []
+  while (pts.length < numberMitochondria) {
+    const i = Math.floor(Math.random() * tessel.length);
+    const p1 = tessel[i];
+    const p2 = tessel[(i + 1) % tessel.length];
+    const t = Math.random();
+    const dx = p2[0] - p1[0]
+    const dy = p2[1] - p1[1]
+    const pt = [p1[0] + t * dx, p1[1] + t * dy]
+    const len = Math.sqrt(dx * dx + dy * dy);
+    const offX = mitochondriaDistance * (dy / len);
+    const offY = mitochondriaDistance * (-dx / len);
+    const offPt = [p1[0] + offX, p2[0] + offY]
+    pts.push(offPt)
+  }
+  for (const pt of pts) {
+    drawCircle(pt, "black", 1)
   }
 }
 
 for (const pt of pts) {
-  drawCircle(pt, ptColors[pts.indexOf(pt)], 2)
+  const r = nucleusRadius;
+  const ptDrawer = new bt.Turtle()
+  const angleStep = 360/pointSteps;
+  const forwardStep = 2 * Math.PI * r / pointSteps;
+  ptDrawer.up();
+  pt[1] = pt[1] - r;
+  ptDrawer.goTo(pt);
+  ptDrawer.down();
+
+  for (let j=0;j<pointSteps;j++) {
+    ptDrawer.forward(forwardStep);
+    ptDrawer.right(angleStep);
+  }
+
+  drawLines(ptDrawer.lines());  
 }
